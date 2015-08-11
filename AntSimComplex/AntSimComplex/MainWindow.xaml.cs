650199d9b20a5da40dbafc0ae73a9a016fccfa5a
William Hallatt <goblincoding@gmail.com>
2015-08-11 15:44:47 +0200

    GUI wip.

--- AntSimComplex/AntSimComplex/MainWindow.xaml.cs
+++ AntSimComplex/AntSimComplex/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TspLibNet;
 
 namespace AntSimComplex
 {
@@ -20,13 +22,34 @@ namespace AntSimComplex
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string _libPath = @"..\..\..\packages\TSPLib.Net.1.1.0\lib\TSPLIB95";
+        private TspLib95 _tspLib;
+
         public MainWindow()
         {
+            CreateTspLib();
             InitializeComponent();
+            PopulateComboBoxes();
         }
 
         private void MenuOpen_Click(object sender, RoutedEventArgs e)
         {
+        }
+
+        private void CreateTspLib()
+        {
+            // Load all symmetric TSP instances.
+            _tspLib = new TspLib95(_libPath);
+            _tspLib.LoadAllTSP();
+        }
+
+        private void PopulateComboBoxes()
+        {
+            this.TSPCombo.ItemsSource = from p in _tspLib.TSPItems()
+                                        where p.Problem.NodeProvider.CountNodes() <= 100
+                                        select p.Problem.Name;
+
+            //this.TSPCombo.ItemsSource = _tspLib.TSPItems().Select(i => i.Problem.Name);
         }
     }
 }
